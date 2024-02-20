97afba7a6666de7d5ad00ae298dc14c479facf40
Yailene Alvarez <yailene.alvarez@gmail.com>
2024-02-20 08:11:01 -0500

    all routing and nav elements added to App.jsx

---
+++ src/components/About/About.jsx
@@ -0,0 +1,16 @@
+
+
+function About() {
+
+    return (
+        <div>
+            <p>This is a GitHub Student List application. A student user can enter their GitHub username
+                in the form, located on the Home page, and hit submit. Upon submission, their username will
+                be stored in the database and uploaded into student list. The student list is shown on the 
+                All Students page. All username input history is displayed on the page.
+            </p>
+        </div>
+    )
+}
+
+export default About
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,19 +2,44 @@ import axios from 'axios';
 import './App.css';
 import StudentForm from '../StudentForm/StudentForm.jsx';
 import StudentList from '../StudentList/StudentList.jsx';
+import About from '../About/About.jsx';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
 
 function App() {
     return (
+        <Router>
         <div className="App">
             <header className="App-header">
             <h1 className="App-title">GitHub Student List</h1>
             </header>
             <br/>
-            <StudentForm />
 
-            <p>Student list:</p>
-            <StudentList />
+            <ul>
+                <li>
+                    <Link to='/home'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+                <li>
+                    <Link to='/allStudents'>All Students</Link>
+                </li>
+            </ul>
+
+            <Route path='/home'>
+                <StudentForm />
+            </Route>
+
+            <Route path='/about'>
+                <About />
+            </Route>
+
+            <Route path='/allStudents'>
+                <p>Student list:</p>
+                <StudentList />
+            </Route>
         </div>
+        </Router>
     );
 }
 
